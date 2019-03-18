7ec8cc0c47e820fa8eab7aed34b546af97a87786
YusukeHata0925 <kma14second@icloud.com>
2019-03-18 05:06:28 +0000

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = Refile.secret_key = 'da9bb63439ec39c8b50434041d71f463701f1ed2d07a63dabdc8f1e0004e2f2c80603034efb51ae1deccf8b424339cead92f013b7db5a934931b62186f2dbfca'
