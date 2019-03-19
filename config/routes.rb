fc0de6de097115c335e0ff11e089c0a3324aa903
YusukeHata0925 <kma14second@icloud.com>
2019-03-19 05:44:17 +0000

    [finish]5-6

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
   end
-    resources :users, only: [:show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :users, only: [:show, :edit, :update]
+    
 end
